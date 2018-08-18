c9f7f465df1bafdbf58a99f9a4da2baaa2a5b38b
readygood <readygood@163.com>
2018-08-19 00:13:13 +0800

    Filebeat

---
+++ Filebeat/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+docker container run --name filebeat --network host \
+-v /root/filebeat/conf/filebeat.yml:/usr/share/filebeat/filebeat.yml \
+-v /var/log/messages:/var/log/messages \
+-v /var/log/nginx/access.log:/var/log/nginx/access.log  \
+--rm -d docker.elastic.co/beats/filebeat:5.6.15 \
+filebeat  -c /usr/share/filebeat/filebeat.yml
